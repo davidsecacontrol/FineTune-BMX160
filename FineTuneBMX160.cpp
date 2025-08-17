1640f24acdfcab66c6923596d6bc1142ab9c7adf
dsecad22 <davidsecacontrol@gmail.com>
2025-08-17 13:19:24 +0200

    Added basic I2C communication handling
    Now it can check if connected, read and write a byte

--- FineTuneBMX160.cpp
+++ FineTuneBMX160.cpp
@@ -1,1 +1,32 @@
 #include "FineTuneBMX160.h"
+
+FTBMX160::FTBMX160(arduino::TwoWire& Wire, uint8_t address) : Wire(Wire), address{address} {};
+
+uint8_t FTBMX160::writeReg(const uint8_t reg, const uint8_t byte)
+{
+    this->Wire.beginTransmission(this->address);
+    this->Wire.write(reg);
+    this->Wire.write(&byte, UINT8_C(1));
+    return this->Wire.endTransmission();
+}
+
+uint8_t FTBMX160::readReg(const uint8_t reg)
+{
+    // Send register to read
+    this->Wire.beginTransmission(this->address);
+    this->Wire.write(&reg, UINT8_C(1));
+    this->Wire.endTransmission(false); // Error to be addressed, "false" to not release bus
+
+    // Read the result
+    this->Wire.requestFrom(this->address, UINT8_C(1));
+    uint8_t byte = this->Wire.read();
+    this->Wire.endTransmission();
+    return byte;
+}
+
+// Add error codes https://docs.arduino.cc/language-reference/en/functions/communication/this->Wire/endTransmission/
+uint8_t FTBMX160::isConnected()
+{
+    this->Wire.beginTransmission(this->address);
+    return this->Wire.endTransmission();
+}
--- FineTuneBMX160.h
+++ FineTuneBMX160.h
@@ -1,6 +1,45 @@
 #ifndef FINETUNE_BMX160
 #define FINETUNE_BMX160
 
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <stdint.h>
+
+class FTBMX160
+{
+public:
+    FTBMX160() = default;
+    FTBMX160(arduino::TwoWire& Wire, uint8_t address = UINT8_C(0x68));
+
+    /**
+     * @brief Sends a write command through the I2C protocol
+     * 
+     * @param reg Reister to write to
+     * @param byte 8-bit value to write
+     * @return uint8_t Status code of Wire.endTrasmission()
+     */
+    uint8_t writeReg(const uint8_t reg, const uint8_t byte);
+
+    /**
+     * @brief Requests a single byte through the I2C protocol
+     * 
+     * @param reg Register to read from
+     * @return uint8_t read byte
+     */
+    uint8_t readReg(const uint8_t reg);
+
+    /**
+     * @brief Returns true if IMU acknowledges conenction
+     * 
+     * @return uint8_t Status code of Wire.endTrasmission()
+     */
+    uint8_t isConnected();
+
+//protected:
+    arduino::TwoWire& Wire = Wire;
+    const uint8_t address= UINT8_C(0x68);
+};
 
 
 
