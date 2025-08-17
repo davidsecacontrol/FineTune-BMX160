9d063e1bc486d5909193d1824e90dce9173d4f38
dsecad22 <davidsecacontrol@gmail.com>
2025-08-17 14:08:25 +0200

    Namespace + erro handling
    Renamed from FTBMX160 to FineTune::BMX160
    Added error handling with I2C_STATUS. Now all functions return I2C_STATUS and must be checked

--- FineTuneBMX160.cpp
+++ FineTuneBMX160.cpp
@@ -1,32 +1,37 @@
 #include "FineTuneBMX160.h"
 
-FTBMX160::FTBMX160(arduino::TwoWire& Wire, uint8_t address) : Wire(Wire), address{address} {};
+using namespace FineTune;
 
-uint8_t FTBMX160::writeReg(const uint8_t reg, const uint8_t byte)
+BMX160::BMX160(arduino::TwoWire& Wire, uint8_t address) : Wire(Wire), address{address} {};
+
+I2C_STATUS BMX160::writeReg(const uint8_t reg, const uint8_t byte)
 {
     this->Wire.beginTransmission(this->address);
     this->Wire.write(reg);
     this->Wire.write(&byte, UINT8_C(1));
-    return this->Wire.endTransmission();
+    return static_cast<I2C_STATUS> (this->Wire.endTransmission());
 }
 
-uint8_t FTBMX160::readReg(const uint8_t reg)
+I2C_STATUS BMX160::readReg(const uint8_t reg, uint8_t& buffer)
 {
     // Send register to read
     this->Wire.beginTransmission(this->address);
     this->Wire.write(&reg, UINT8_C(1));
-    this->Wire.endTransmission(false); // Error to be addressed, "false" to not release bus
+    I2C_STATUS status = static_cast<I2C_STATUS>(this->Wire.endTransmission(false)); // Error to be addressed, "false" to not release bus
+
+    if(status != I2C_STATUS::SUCCESS){
+        return status;
+    }
 
     // Read the result
     this->Wire.requestFrom(this->address, UINT8_C(1));
-    uint8_t byte = this->Wire.read();
-    this->Wire.endTransmission();
-    return byte;
+    buffer = this->Wire.read();
+    return static_cast<I2C_STATUS> (this->Wire.endTransmission());
 }
 
 // Add error codes https://docs.arduino.cc/language-reference/en/functions/communication/this->Wire/endTransmission/
-uint8_t FTBMX160::isConnected()
+I2C_STATUS BMX160::isConnected()
 {
     this->Wire.beginTransmission(this->address);
-    return this->Wire.endTransmission();
+    return static_cast<I2C_STATUS> (this->Wire.endTransmission());
 }
--- FineTuneBMX160.h
+++ FineTuneBMX160.h
@@ -6,41 +6,54 @@
 #include <SPI.h>
 #include <stdint.h>
 
-class FTBMX160
+namespace FineTune
 {
-public:
-    FTBMX160() = default;
-    FTBMX160(arduino::TwoWire& Wire, uint8_t address = UINT8_C(0x68));
+    enum struct I2C_STATUS : uint8_t
+    {
+        SUCCESS = UINT8_C(0),
+        ERR_TOO_LONG_FOR_BUFFER = UINT8_C(1),
+        ERR_NACK_ON_ADDRESS = UINT8_C(2),
+        ERR_NACK_ON_DATA_TRANSMISSION = UINT8_C(3),
+        ERR_OTHER = UINT8_C(4),
+        ERR_TIMEOUT = UINT8_C(5)
+    };
 
-    /**
-     * @brief Sends a write command through the I2C protocol
-     * 
-     * @param reg Reister to write to
-     * @param byte 8-bit value to write
-     * @return uint8_t Status code of Wire.endTrasmission()
-     */
-    uint8_t writeReg(const uint8_t reg, const uint8_t byte);
+    class BMX160
+    {
+    public:
+        BMX160() = default;
+        BMX160(arduino::TwoWire &Wire, uint8_t address = UINT8_C(0x68));
 
-    /**
-     * @brief Requests a single byte through the I2C protocol
-     * 
-     * @param reg Register to read from
-     * @return uint8_t read byte
-     */
-    uint8_t readReg(const uint8_t reg);
+        /**
+         * @brief Sends a write command through the I2C protocol
+         *
+         * @param reg Reister to write to
+         * @param byte 8-bit value to write
+         * @return uint8_t FineTune::I2CStatus to identify failed transmission
+         */
+        [[nodiscard]] I2C_STATUS writeReg(const uint8_t reg, const uint8_t byte);
 
-    /**
-     * @brief Returns true if IMU acknowledges conenction
-     * 
-     * @return uint8_t Status code of Wire.endTrasmission()
-     */
-    uint8_t isConnected();
+        /**
+         * @brief Requests a single byte through the I2C protocol
+         *
+         * @param reg Register to read from
+         * @param buffer 8-bit variable to write to
+         * @return uint8_t FineTune::I2CStatus to identify failed transmission
+         */
+        [[nodiscard]] I2C_STATUS readReg(const uint8_t reg, uint8_t& buffer);
 
-//protected:
-    arduino::TwoWire& Wire = Wire;
-    const uint8_t address= UINT8_C(0x68);
-};
+        /**
+         * @brief Returns true if IMU acknowledges conenction
+         *
+         * @return uint8_t FineTune::I2CStatus to identify failed transmission
+         */
+        [[nodiscard]] I2C_STATUS isConnected();
 
+        // protected:
+        arduino::TwoWire &Wire = Wire;
+        const uint8_t address = UINT8_C(0x68);
+    };
 
+}
 
 #endif
