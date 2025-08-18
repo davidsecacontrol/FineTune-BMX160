563ca83287b35551eda48711c758968d6fbfef29
dsecad22 <davidsecacontrol@gmail.com>
2025-08-18 17:19:23 +0200

    Auto-spacing, 0 code change

--- FineTuneBMX160.cpp
+++ FineTuneBMX160.cpp
@@ -11,39 +11,35 @@ constexpr float G_TO_MS2 = 9.80665f;
 
 namespace SENSITIVITY
 {
-    constexpr float ACCEL[] = { 
+    constexpr float ACCEL[] = {
         1.0f / 16384,
         1.0f / 8192,
         1.0f / 4096,
-        1.0f / 2048
-    };
-    
-    constexpr float GYRO[] = {  
-        1.0f/16.4f,
-        1.0f/32.8f,
-        1.0f/65.6f,
-        1.0f/131.2f
-    };
+        1.0f / 2048};
+
+    constexpr float GYRO[] = {
+        1.0f / 16.4f,
+        1.0f / 32.8f,
+        1.0f / 65.6f,
+        1.0f / 131.2f};
 
     constexpr float MAGN[] = {
-        0.3f
-    };
+        0.3f};
 }
 
-namespace MASK{
+namespace MASK
+{
     constexpr uint8_t ACCEL_RANGE[] = {
         0b00000011,
         0b00000101,
         0b00001000,
-        0b00001100
-    };
+        0b00001100};
     constexpr uint8_t GYRO_RANGE[] = {
         0b00000000,
         0b00000001,
         0b00000010,
         0b00000011,
-        0b00000100
-    };
+        0b00000100};
 }
 
 BMX160::BMX160(arduino::TwoWire &Wire, uint8_t address) : Wire(Wire), address{address} {};
@@ -76,35 +72,36 @@ I2C_STATUS BMX160::begin()
     return state;
 }
 
-I2C_STATUS BMX160::setAccelRange(RANGE::ACCEL range){
-    I2C_STATUS state = writeReg(REGISTER::ACC_RANGE,MASK::ACCEL_RANGE[(int) range]);
-    
-    if(state != I2C_STATUS::SUCCESS){
+I2C_STATUS BMX160::setAccelRange(RANGE::ACCEL range)
+{
+    I2C_STATUS state = writeReg(REGISTER::ACC_RANGE, MASK::ACCEL_RANGE[(int)range]);
+
+    if (state != I2C_STATUS::SUCCESS)
+    {
         return state;
     }
     this->accelerometer_range = range;
 
     // Recommended to perform a read to remove all stall data
     DataPacket buffer;
-    return getAllData(buffer,buffer,buffer);
-
+    return getAllData(buffer, buffer, buffer);
 }
 
-I2C_STATUS BMX160::setGyroRange(RANGE::GYRO range){
+I2C_STATUS BMX160::setGyroRange(RANGE::GYRO range)
+{
 
-    I2C_STATUS state = writeReg(REGISTER::GYR_RANGE,MASK::GYRO_RANGE[(int) range]);
+    I2C_STATUS state = writeReg(REGISTER::GYR_RANGE, MASK::GYRO_RANGE[(int)range]);
 
-    if(state != I2C_STATUS::SUCCESS){
+    if (state != I2C_STATUS::SUCCESS)
+    {
         return state;
     }
     this->gyroscope_range = range;
 
     // Recommended to perform a read to remove all stall data
     DataPacket buffer;
-    return getAllData(buffer,buffer,buffer);
-
+    return getAllData(buffer, buffer, buffer);
 }
-
 
 I2C_STATUS BMX160::getAllData(DataPacket &accel, DataPacket &gyro, DataPacket &magn)
 {
@@ -116,9 +113,9 @@ I2C_STATUS BMX160::getAllData(DataPacket &accel, DataPacket &gyro, DataPacket &m
         return state;
     }
 
-    CopyBufferToDataPacket(accel, &buffer[14], SENSITIVITY::ACCEL[(int) this->accelerometer_range]*G_TO_MS2);
-    CopyBufferToDataPacket(gyro,  &buffer[8] , SENSITIVITY::GYRO[(int) this->gyroscope_range]);
-    CopyBufferToDataPacket(magn,  &buffer[0] , SENSITIVITY::MAGN[(int)this->magnetorquer_range]);
+    CopyBufferToDataPacket(accel, &buffer[14], SENSITIVITY::ACCEL[(int)this->accelerometer_range] * G_TO_MS2);
+    CopyBufferToDataPacket(gyro, &buffer[8], SENSITIVITY::GYRO[(int)this->gyroscope_range]);
+    CopyBufferToDataPacket(magn, &buffer[0], SENSITIVITY::MAGN[(int)this->magnetorquer_range]);
 
     return state;
 }
@@ -126,7 +123,7 @@ I2C_STATUS BMX160::getAllData(DataPacket &accel, DataPacket &gyro, DataPacket &m
 I2C_STATUS BMX160::writeReg(const REGISTER reg, const uint8_t byte)
 {
     this->Wire.beginTransmission(this->address);
-    this->Wire.write((uint8_t) reg);
+    this->Wire.write((uint8_t)reg);
     this->Wire.write(&byte, 1);
     return static_cast<I2C_STATUS>(this->Wire.endTransmission());
 }
@@ -140,7 +137,7 @@ I2C_STATUS BMX160::readReg(const REGISTER reg, uint8_t *const buffer, int length
 {
     // Send register to read
     this->Wire.beginTransmission(this->address);
-    this->Wire.write(reinterpret_cast<const uint8_t*>(&reg), 1);
+    this->Wire.write(reinterpret_cast<const uint8_t *>(&reg), 1);
     I2C_STATUS status = static_cast<I2C_STATUS>(this->Wire.endTransmission(false)); // Error to be addressed, "false" to not release bus
 
     if (status != I2C_STATUS::SUCCESS)
